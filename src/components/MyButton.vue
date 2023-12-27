391b18efa55981c630bf2cfd68a55c2714339721
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-12-28 02:06:40 +0530

    Created Component which needs to be exported as Custom Component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    @click="handleApiCall()"
+    :style="{
+      padding: '15px',
+      fontSize: '20px',
+      background: background,
+      color: color,
+    }"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  props: {
+    background: {
+      default: "#456099",
+    },
+    color: {
+      default: "#fff",
+    },
+    text: {
+      default: "My Button",
+    },
+  },
+  methods: {
+    async handleApiCall() {
+      console.log("Triggering a ApI on click event");
+      await fetch("https://fakestoreapi.com/products/1")
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+    },
+  },
+};
+</script>
